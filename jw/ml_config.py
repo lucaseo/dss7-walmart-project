279d5c6eeb9193f834fca8743c52ae2263680d1a
JW <anylee2142@gmail.com>
2018-03-11 17:54:50 +0900

    180311 Missing filled, couple of experiments

--- jw/ml_config.py
+++ jw/ml_config.py
@@ -11,6 +11,8 @@ import matplotlib as mpl
 import matplotlib.pylab as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
+from sklearn.datasets import make_regression
+
 sns.set()
 sns.set_style("whitegrid")
 sns.set_color_codes()
